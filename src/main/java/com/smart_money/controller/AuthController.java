dd1c3b9e7f80d6d5fd4c900b97a715e62a9a5287
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-03-27 19:36:19 +0100

    :memo: created register and findAllUsers endpoints
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

--- src/main/java/com/smart_money/controller/AuthController.java
+++ src/main/java/com/smart_money/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.smart_money.controller;
 
+import com.smart_money.dto.reponse.ResponseDTO;
+import com.smart_money.dto.request.RegisterUserDTO;
+import com.smart_money.model.User;
 import com.smart_money.repository.UserRepository;
 import com.smart_money.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +27,22 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    
+    public ResponseEntity<ResponseDTO> registerUser(@Valid @RequestBody RegisterUserDTO body) {
+        if (userRepository.findUserByEmail(body.email()).isPresent()) {
+            return ResponseEntity.status(400).body(new ResponseDTO(false, null, "Email already registered."));
+        }
 
+        User newUser = new User();
+        newUser.setName(body.name());
+        newUser.setLastName(body.lastName());
+        newUser.setEmail(body.email());
+        newUser.setPassword(passwordEncoder.encode(body.password()));
+        newUser.setAge(body.age());
 
+        userRepository.save(newUser);
 
+        String token = tokenService.generateToken(newUser);
 
+        return ResponseEntity.ok(new ResponseDTO(true, token, "Registered successfully!"));
+    }
 }
