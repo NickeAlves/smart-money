c30bf4b1699034158e19475a36c78d62e6048b42
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-03-29 13:25:37 +0100

    :page_facing_up: feat(frontend) adjusted home page
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,37 +8,6 @@ export default function Home() {
       <main className="pt-20 min-h-screen bg-gray-900">
         <div className="container mx-auto px-4 py-8">
           <Header />
-          <div>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-
-            <p className="text-3xl">jfnhduiasghfa</p>
-
-            <p className="text-3xl">jfnhduiasghfa</p>
-
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-            <p className="text-3xl">jfnhduiasghfa</p>
-          </div>
         </div>
       </main>
     </>
